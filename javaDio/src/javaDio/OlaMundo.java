b529e01384844096d2b877ac5ed9868f702b639e
RafaelDuke <rafaelfuriads@hotmail.com>
2024-07-30 00:16:35 -0300

    Teste Idade

--- javaDio/src/javaDio/OlaMundo.java
+++ javaDio/src/javaDio/OlaMundo.java
@@ -1,12 +1,26 @@
 package javaDio;
 
+import java.util.Scanner;
+
 public class OlaMundo {
 
 	public static void main(String []args) {
 		System.out.println("Olá Mundo");
 		System.out.println("Olá Mundoo");
 		System.out.println("Finalmente consegui");
+		
+		int a = 2024;
+		int b;
+		int result;
+		
+		Scanner leia = new Scanner(System.in);	
 
+		System.out.println("Informe a sua data de nascimento");
+		b = leia.nextInt();
+		System.out.println("Sua idade é: ");
+		result = (a - b);
+		System.out.println(result);
+		
 		
 	}
 	
